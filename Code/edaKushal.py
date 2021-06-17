4f2bcaacdfec787f5e46956e272c761234885cdc
mdk312gwu <85844576+mdk312gwu@users.noreply.github.com>
2021-06-17 15:30:21 -0400

    Added correlation function and gridsearch updates

--- Code/edaKushal.py
+++ Code/edaKushal.py
@@ -5,8 +5,47 @@ import matplotlib.pyplot as plt
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
 from sklearn.neural_network import MLPRegressor
-from sklearn.impute import SimpleImputer
-import seaborn as sns
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputerimport seaborn as sns
+
+# Functions
+def findCorrelations(correlations, cutoff=0.9):
+    corr_mat = abs(correlations)
+    varnum = corr_mat.shape[1]
+    original_order = np.arange(0, varnum + 1, 1)
+    tmp = corr_mat.copy(deep=True)
+    np.fill_diagonal(tmp.values, np.nan)
+    maxAbsCorOrder = tmp.apply(np.nanmean, axis=1)
+    maxAbsCorOrder = (-maxAbsCorOrder).argsort().values
+    corr_mat = corr_mat.iloc[list(maxAbsCorOrder), list(maxAbsCorOrder)]
+    newOrder = original_order[list(maxAbsCorOrder)]
+    del (tmp)
+    deletecol = np.repeat(False, varnum)
+    x2 = corr_mat.copy(deep=True)
+    np.fill_diagonal(x2.values, np.nan)
+    for i in range(varnum):
+        if not (x2[x2.notnull()] > 0.9).any().any():
+            print('No correlations above threshold')
+            break
+        if deletecol[i]:
+            continue
+        for j in np.arange(i + 1, varnum, 1):
+            if (not deletecol[i] and not deletecol[j]):
+                if (corr_mat.iloc[i, j] > cutoff):
+                    mn1 = np.nanmean(x2.iloc[i,])
+                    mn2 = np.nanmean(x2.drop(labels=x2.index[j], axis=0).values)
+                    if (mn1 > mn2):
+                        deletecol[i] = True
+                        x2.iloc[i, :] = np.nan
+                        x2.iloc[:, i] = np.nan
+                    else:
+                        deletecol[j] = True
+                        x2.iloc[j, :] = np.nan
+                        x2.iloc[:, j] = np.nan
+    newOrder = [i for i, x in enumerate(deletecol) if x]
+    return(newOrder)
+
+
 
 # Declare an empty list to store each line
 lines = []
@@ -72,8 +111,6 @@ sns.heatmap(corrage, annot=True)
 plt.show()
 
 
-
-
 #check columns with over 50% missing values
 print(missing.where(missing>50))
 #check for total null values in df
@@ -81,18 +118,6 @@ print(df.isnull().sum().sum())
 #drop non predictive fields
 df = df.drop(columns =['state','county','communityname','community','fold',])
 #create dataset that drops columns where 50% of values are null
-dfdrop = df.dropna(axis=1,thresh=997)
-#impute values with mean for full dataset
-imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
-imp_mean.fit(df)
-SimpleImputer()
-dfimpmean = imp_mean.transform(df)
-#impute values with mean for full dataset
-imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
-imp_mean.fit(dfdrop)
-SimpleImputer()
-dfdropimpmean = imp_mean.transform(dfdrop)
-
 
 plt.figure(2)
 plt.plot(df['ViolentCrimesPerPop'], 'o')
@@ -101,13 +126,28 @@ plt.ylabel('Total number of violent crimes per 100K population')
 plt.xlabel('Community index')
 plt.savefig('violent.png')
 
-piped = Pipeline([('mlp', MLPRegressor(random_state=1))])
-max_neurons = 20
-max_layers = 5
+# Impute missing values
 
+X = df.drop('ViolentCrimesPerPop', axis=1)
+y = df['ViolentCrimesPerPop']
+
+# imp = IterativeImputer(max_iter=10, random_state=0)
+# Ximpute = imp.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+# Highly correlated features
+to_remove = findCorrelations(X_train.corr())
+X_train = X_train.drop(X_train.columns[to_remove], axis=1)
+# Drop police columns with NA values
+X_train = X_train.dropna(axis=1)
+
+piped = Pipeline([('mlp', MLPRegressor(random_state=1))])
+max_neurons = 100
+max_layers = 5
 params = []
-for i in range(max_layers):
-    for j in range(max_neurons):
+for i in np.arange(2, max_layers + 1, 1):
+    for j in np.arange(5, max_neurons + 1, 5):
         out = tuple(np.repeat(j + 1, i + 1))
         params.append(out)
 
@@ -117,8 +157,9 @@ param_grid = [{'mlp__activation': ['logistic', 'tanh', 'relu'],
 gs = GridSearchCV(estimator=piped,
                   param_grid=param_grid,
                   scoring='neg_mean_squared_error',
+                  verbose=2,
                   cv=5)
 
-# gs = gs.fit(X_train, y_train)
-# print(gs.best_score_)
-# print(gs.best_params_)
+gs = gs.fit(X_train, y_train)
+print(gs.best_score_)
+print(gs.best_params_)
