a23bb9d1f53639b39d6a8c7d9e30d2a871ab0046
Kushal Ismael <Kushal.Ismael@gmail.com>
2021-06-19 13:03:25 -0400

    Imputed results

--- Code/mlmodels.py
+++ Code/mlmodels.py
@@ -1,9 +1,9 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import svm, tree, neighbors, neural_network, ensemble
 from sklearn.metrics import mean_squared_error
-
 
 
 def modeleval(X,y,test):
@@ -13,8 +13,8 @@ def modeleval(X,y,test):
     :param X: predictor variables
     :param y: target variable
     :param test: float of amount to test
-    :return: table of model with corresponding R**2 and MSE
 
+    :return: table of model with corresponding R**2 and MSE
     """
 
     #--train test split--
@@ -27,12 +27,19 @@ def modeleval(X,y,test):
     svrscore = svr.score(y_test,y_pred)
     svrmse = mean_squared_error(y_test,y_pred)
 
-    #--KNR--
-    knr = neighbors.KNeighborsRegressor(n_neighbors=3)
-    knr.fit(X_train,y_train)
-    y_pred = knr.predict(X_test)
-    knrscore = knr.score(y_test,y_pred)
-    knrmse = mean_squared_error(y_test, y_pred)
+    #--KNR3--
+    knr3 = neighbors.KNeighborsRegressor(n_neighbors=3)
+    knr3.fit(X_train,y_train)
+    y_pred = knr3.predict(X_test)
+    knr3score = knr3.score(y_test,y_pred)
+    knr3mse = mean_squared_error(y_test, y_pred)
+
+    #--KNR5--
+    knr5 = neighbors.KNeighborsRegressor(n_neighbors=5)
+    knr5.fit(X_train,y_train)
+    y_pred = knr5.predict(X_test)
+    knr5score = knr5.score(y_test,y_pred)
+    knr5mse = mean_squared_error(y_test, y_pred)
 
     #--DTR--
     dtr = tree.DecisionTreeRegressor()
@@ -48,12 +55,16 @@ def modeleval(X,y,test):
     rfrscore = rfr.score(y_test,y_pred)
     rfrmse = mean_squared_error(y_test,y_pred)
 
+    #--table of results--
     evaltable = pd.DataFrame({
-        'Model':['SVR','KNR','DTR','RFR','MLP'], # add any other
-        'Accuracy Score': [svrscore,knrscore,dtrscore,rfrscore],
-        'MSE':[svrmse,knrmse,dtrmse,rfrmse]
+        'Model':['SVR','KNR3','KNR5','DTR','RFR','MLP'],
+        'Accuracy Score': [svrscore, knr3score, knr5score, dtrscore, rfrscore],
+        'MSE':[svrmse,knr3mse, knr5mse, dtrmse, rfrmse]
     })
 
-
-
     return evaltable.style.background_gradient(cmap='Blues')
+
+
+plt.fig(0)
+plt.hist(y,bins=20, label='y')
+plt.hist(y_pred,bins=20,label='y_pred')
